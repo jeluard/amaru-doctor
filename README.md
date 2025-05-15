0a11a3cee57982a149843852aa13cc07c8b93559
jeluard <jeluard@users.noreply.github.com>
2025-05-15 10:06:55 +0200

    chore: initial commit
    Signed-off-by: jeluard <jeluard@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+# amaru-doctor
+
+[![CI](https://github.com//amaru-doctor/workflows/CI/badge.svg)](https://github.com/jeluard/amaru-doctor/actions)
+
+A TUI for amaru
+
+
+Run with `AMARU_LEDGER_DB=path/to/ledger.db cargo run`
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::{env, path::Path};
+
+use amaru_kernel::network::NetworkName;
+use amaru_ledger::store::ReadOnlyStore;
+use amaru_stores::rocksdb::RocksDB;
+use clap::Parser;
+use cli::Cli;
+use color_eyre::Result;
+
+use crate::app::App;
+
+mod action;
+mod app;
+mod cli;
+mod components;
+mod config;
+mod errors;
+mod logging;
+mod tui;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let db = RocksDB::new(Path::new(&env::var("AMARU_LEDGER_DB")?), NetworkName::Preprod.into())?;
+
+    let pots = db.pots()?;
+    println!("pots: {:?}", pots);
+    
+    crate::errors::init()?;
+    crate::logging::init()?;
+
+    let args = Cli::parse();
+    let mut app = App::new(args.tick_rate, args.frame_rate)?;
+    app.run().await?;
+    Ok(())
+}
