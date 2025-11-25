41a36d0e11b95c465c15dfbbfc6f34b94ce6b5a9
Geoff Little <g.little712@gmail.com>
2025-11-25 14:33:09 +0000

    refactor: move prometheus component into its page

--- src/update/prom_metrics.rs
+++ src/update/prom_metrics.rs
@@ -1,11 +1,19 @@
-use crate::{app_state::AppState, states::Action, update::Update};
+use crate::{
+    app_state::AppState,
+    components::prometheus_page::PrometheusPageComponent,
+    states::{Action, ComponentId},
+    update::Update,
+};
 
 /// The Update fn for sync'ing Prometheus metrics.
 pub struct PromMetricsUpdate;
 impl Update for PromMetricsUpdate {
     fn update(&self, a: &Action, s: &mut AppState) -> Vec<Action> {
-        if *a == Action::SyncPromMetrics {
-            s.get_prom_metrics_mut().sync();
+        if *a == Action::SyncPromMetrics
+            && let Some(page) = s.component_registry.get_mut(&ComponentId::PrometheusPage)
+            && let Some(prom_page) = page.as_any_mut().downcast_mut::<PrometheusPageComponent>()
+        {
+            prom_page.metrics.sync();
         }
 
         Vec::new()
