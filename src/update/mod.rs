4128e0e4985bba39603e77dda7e0b9203205089b
Geoff Little <g.little712@gmail.com>
2025-12-01 22:07:48 +0000

    remove select span update

--- src/update/mod.rs
+++ src/update/mod.rs
@@ -5,7 +5,7 @@ use crate::{
     update::{
         button::GetButtonEventsUpdate, focus::FocusUpdate, layout::LayoutUpdate,
         mouse::MouseEventUpdate, mouse_click::MouseClickUpdate, mouse_scroll::MouseScrollUpdate,
-        scroll::ScrollUpdate, select_span::SelectSpanUpdate, tabs::TabsUpdate, tick::TickUpdate,
+        scroll::ScrollUpdate, tabs::TabsUpdate, tick::TickUpdate,
     },
 };
 
@@ -16,7 +16,6 @@ pub mod mouse;
 pub mod mouse_click;
 pub mod mouse_scroll;
 pub mod scroll;
-pub mod select_span;
 pub mod tabs;
 pub mod tick;
 
@@ -36,7 +35,6 @@ pub static UPDATE_DEFS: &[&dyn Update] = &[
     &ScrollUpdate,
     &LayoutUpdate,
     &TickUpdate,
-    &SelectSpanUpdate,
     &GetButtonEventsUpdate,
     &MouseEventUpdate,
     &MouseClickUpdate,
